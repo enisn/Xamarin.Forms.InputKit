5b1f6158336e5fc9d41db613b1c9e4216d29ba37
enisn <iletisim@enisnecipoglu.com>
2019-03-02 13:47:21 +0300

    CheckBox updated with custom images

---
+++ InputKit/Shared/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.InputKit.Shared
+{
+    public class Constants
+    {
+        public const string RESOURCE_CHECK = "Plugin.InputKit.Shared.Resources.check.png";
+    }
+}
