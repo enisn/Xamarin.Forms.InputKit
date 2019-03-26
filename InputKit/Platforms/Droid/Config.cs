a19e6e90656946eb55187d8b3c5c0af92f27ea17
enisn <iletisim@enisnecipoglu.com>
2019-03-26 14:26:15 +0300

    Fixed dropdown crash on Android

--- InputKit/Platforms/Droid/Config.cs
+++ InputKit/Platforms/Droid/Config.cs
@@ -13,18 +13,20 @@ namespace Plugin.InputKit.Platforms.Droid
 
         public static Activity CurrentActivity
         {
-            get => currentActivity ?? throw new NotImplementedException("Android wasn't initialized! Make sure your MainActivity > OnCreate() contains Plugin.InputKit.Platforms.Droid.Config.Init(this,savedInstance)");
+            get => currentActivity ?? throw new NotImplementedException("Android wasn't initialized! Make sure your MainActivity > OnCreate() contains Plugin.InputKit.Platforms.Droid.Config.Init(this, savedInstanceState)");
             private set => currentActivity = value;
         }
 
         public static Bundle CurrentBundle
         {
-            get => currentBundle ?? throw new NotImplementedException("Android wasn't initialized! Make sure your MainActivity > OnCreate() contains Plugin.InputKit.Platforms.Droid.Config.Init(this,savedInstance)");
+            get => currentBundle ?? throw new NotImplementedException("Android wasn't initialized! Make sure your MainActivity > OnCreate() contains Plugin.InputKit.Platforms.Droid.Config.Init(this, savedInstanceState)");
             set => currentBundle = value;
         }
 
         public static void Init(Activity activity, Bundle bundle)
         {
+            CurrentActivity = activity;
+            CurrentBundle = bundle;
             Console.WriteLine($"[{typeof(AutoCompleteViewRenderer).FullName}] Initialized.");
             Console.WriteLine($"[{typeof(EmptyEntryRenderer).FullName}] Initialized.");
             Console.WriteLine($"[{typeof(MenuEffect).FullName}] Initialized.");
