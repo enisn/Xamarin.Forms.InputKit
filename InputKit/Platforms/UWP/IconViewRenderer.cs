25627ce4451b27adcd313c229f5d52b416e15218
enisn <info@enisnecipoglu.com>
2021-11-12 02:00:14 +0300

    Revert "Merge branch 'feature/maui-update-experimental' into feature/maui-update"
    This reverts commit 490fd115053127bed0293fa61a94751c73e9fece, reversing
    changes made to 5cef4afcc64cab236127bd9deaeca4008940646e.

---
+++ InputKit/Platforms/UWP/IconViewRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Plugin.InputKit.Platforms.UWP;
+using Plugin.InputKit.Shared.Controls;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ExportRenderer(typeof(IconView), typeof(IconViewRenderer))]
+namespace Plugin.InputKit.Platforms.UWP
+{
+    public class IconViewRenderer : ViewRenderer<IconView, BitmapIcon>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<IconView> e)
+        {
+            if (Control != null)
+            {
+                var brush = new ImageBrush();
+                if (e?.NewElement != null)
+                    brush.ImageSource = new BitmapImage(new Uri(e.NewElement.Source.ToString(), UriKind.Relative));
+
+                Control.Foreground = brush;
+            }
+            base.OnElementChanged(e);
+        }
+    }
+}
---
+++ InputKit/Platforms/WatchOS/InputKitImplementation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Plugin.InputKit
+{
+    /// <summary>
+    /// Interface for $safeprojectgroupname$
+    /// </summary>
+    public class InputKitImplementation : IInputKit
+    {
+    }
+}
