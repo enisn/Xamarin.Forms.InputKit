43d503063a9761e77399a1bed97a5253d2c8bb4b
enisn <info@enisnecipoglu.com>
2022-03-11 23:03:19 +0300

    Add windows target-framework

---
+++ src/InputKit/Platforms/Windows/Handlers/IconViewHandler.Windows.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Maui.Handlers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputKit.Handlers.IconView
+{
+    public partial class IconViewHandler : ViewHandler<IIconViewHandler, Microsoft.UI.Xaml.Controls.BitmapIcon>
+    {
+        
+    }
+}
