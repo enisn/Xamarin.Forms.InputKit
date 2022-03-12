33c7f1c500b5d918a550d0d9d5bac8aa1d81ab72
enisn <info@enisnecipoglu.com>
2022-03-12 13:23:29 +0300

    Update DemoApp

--- sandbox/SandboxMAUI/App.xaml.cs
+++ sandbox/SandboxMAUI/App.xaml.cs
@@ -1,17 +1,11 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Controls;
-using Microsoft.Maui.Controls.PlatformConfiguration.WindowsSpecific;
-using Application = Microsoft.Maui.Controls.Application;
+﻿namespace SandboxMAUI;
 
-namespace SandboxMAUI
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
+        InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
-        }
+        MainPage = new NavigationPage(new MainPage());
     }
 }
--- sandbox/SandboxMAUI/MauiProgram.cs
+++ sandbox/SandboxMAUI/MauiProgram.cs
@@ -1,28 +1,18 @@
-﻿using Microsoft.Maui;
-using Microsoft.Maui.Hosting;
-using Microsoft.Maui.Controls.Hosting;
-using InputKit.Handlers;
-using InputKit.Shared.Controls;
+﻿namespace SandboxMAUI;
 
-namespace SandboxMAUI
+public static class MauiProgram
 {
-    public static class MauiProgram
+	public static MauiApp CreateMauiApp()
 	{
-		public static MauiApp CreateMauiApp()
-		{
-			var builder = MauiApp.CreateBuilder();
-			builder
-				.UseMauiApp<App>()
-				.ConfigureMauiHandlers(handlers =>
-                {
-					handlers.AddInputKitHandlers();
-                })
-				.ConfigureFonts(fonts =>
-				{
-					fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				});
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
 
-			return builder.Build();
-		}
+
+		return builder.Build();
 	}
 }
