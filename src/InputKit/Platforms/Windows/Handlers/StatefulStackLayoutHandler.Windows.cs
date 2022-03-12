d3b13452fb1e2a6cfa5480392e6420f84c640832
enisn <info@enisnecipoglu.com>
2022-03-12 13:23:09 +0300

    Create StatefulStackLayoutHandler.Windows.cs

---
+++ src/InputKit/Platforms/Windows/Handlers/StatefulStackLayoutHandler.Windows.cs
@@ -0,0 +1,15 @@
+﻿#if UWP
+using Microsoft.Maui.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputKit.Handlers
+{
+    public partial class StatefulStackLayoutHandler
+    {
+    }
+}
+#endif
