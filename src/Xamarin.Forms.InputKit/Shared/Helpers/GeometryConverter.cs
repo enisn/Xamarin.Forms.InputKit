c1e844ba94cd894c7266519def1aaac05f2dcd47
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-08-28 13:17:05 +0300

    Add drawing infrastructure to XF

---
+++ src/Xamarin.Forms.InputKit/Shared/Helpers/GeometryConverter.cs
@@ -0,0 +1,14 @@
+﻿
+using Xamarin.Forms.Shapes;
+
+namespace Plugin.InputKit.Shared.Helpers
+{
+    public static class GeometryConverter
+    {
+        private static PathGeometryConverter PathGeometryConverter { get; } = new PathGeometryConverter();
+        public static Geometry FromPath(string path)
+        {
+            return (Geometry)PathGeometryConverter.ConvertFromInvariantString(path);
+        }
+    }
+}
