07972464bc2d23854c62d853fc78af21610b9061
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-03 21:50:21 +0300

    Add Material icons & Update demo

--- sandbox/SandboxMAUI/MauiProgram.cs
+++ sandbox/SandboxMAUI/MauiProgram.cs
@@ -14,6 +14,7 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("MaterialIcons-Regular.ttf", "MaterialIcon");
 			});
 
 		builder.ConfigureMauiHandlers(handlers =>
